9113e17e7b68eb86517ac573b5bbe2f26ec5fa0a
mina bessada <mina.adibe@gmail.com>
2022-09-09 23:56:21 +0200

    feat(project): implement project theme - add font familly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: [
+    "./src/components/**/*.js",
+    "./src/components/**/*.jsx",
+    "./src/pages/**/*.js",
+    "./src/pages/**/*.jsx",
+  ],
+
+  content: ["./src/**/*.{jsx,js}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ["Lato", "sans-serif"],
+    },
+
+    colors: {
+      c000: "#ffffff", //white
+      c100: "#000000", // black
+      c200: "#4894FE", // primary blue color
+      c300: "#E1E1E1", // borders - dividers
+    },
+    fontSize: {
+      xxs: "0.875rem", //14px
+      xs: "0.938rem", // 15px
+      sm: "1rem", // 16px
+      base: "1.063rem", // 17px
+      md: "1.125rem", // 18px
+      lg: "1.563rem", //25px
+    },
+    extend: {
+      boxShadow: {
+        btn: "0px 1px 4px rgba(0, 0, 0, 0.161)",
+      },
+    },
   },
   plugins: [],
 };
