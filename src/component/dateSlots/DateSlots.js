9df673b7610fbbf3d57a49256fdf2dabf85ae147
mina bessada <mina.adibe@gmail.com>
2022-09-10 06:43:43 +0200

    feat(dateslot): implement DateSlots component

---
+++ src/component/dateSlots/DateSlots.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import Slots from "../slots/Slots";
+
+const DateSlots = ({ doctorInfo, availableTime }) => {
+  return (
+    <div>
+      <p>Schedule</p>
+      <div className="flex flex-row gap-2 ">
+        <Swiper
+          slidesPerView={3}
+          centeredSlides={true}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper">
+          {doctorInfo?.schedule?.length > 0 &&
+            doctorInfo?.schedule?.map((dateSlot) => {
+              return (
+                <SwiperSlide
+                  key={dateSlot.availability.date}
+                  onClick={() => availableTime(dateSlot)}>
+                  <Slots>
+                    {dateSlot.availability.date + dateSlot.availability.day}
+                  </Slots>
+                </SwiperSlide>
+              );
+            })}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default DateSlots;
+
+DateSlots.propTypes = {
+  availableTime: PropTypes.func,
+  doctorInfo: PropTypes.object,
+};
